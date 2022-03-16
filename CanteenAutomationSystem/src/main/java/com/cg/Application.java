cebccaed97ef966e03db9df64351c5f67fece2a6
mohdhafe <mohdhafe@INLEN8520041983.corp.capgemini.com>
2022-03-16 12:50:38 +0530

    # WARNING: head commit changed in the meantime
    First Commit

---
+++ CanteenAutomationSystem/src/main/java/com/cg/Application.java
@@ -0,0 +1,11 @@
+package com.cg;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication//it is referring for spring boot application
+public class Application {
+
+		public static void main(String[] args) {//main method
+			SpringApplication.run(Application.class, args);
+		}
+
+	}
